2bd88d9b353465edb6588eb2d9e4e37d5b07e206
Mayank Rai <mayankrai271993@gmail.com>
2022-07-29 17:17:10 +0200

    Exposes adaptiveWindowType in build context extension

--- lib/utils/extension/build_context_extension.dart
+++ lib/utils/extension/build_context_extension.dart
@@ -1,9 +1,12 @@
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 import 'package:component_library/theme/cl_theme.dart';
 import 'package:component_library/theme/cl_theme_widget.dart';
 import 'package:flutter/material.dart';
 
 extension BuildContextExtension on BuildContext {
   CLTheme get clTheme => CLThemeWidget.of(this);
+
+  AdaptiveWindowType get adaptiveWindowType => getWindowType(this);
 
   bool get isIOS => Theme.of(this).platform == TargetPlatform.iOS;
 
